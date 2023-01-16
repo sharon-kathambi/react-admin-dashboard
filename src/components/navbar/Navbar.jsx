df9e1cdbbdd0c7c85cbb2d82dc62362ec9094895
Sharon Mwenda <smkathambi@gmail.com>
2023-01-16 15:52:14 +0300

    styled and created navbar component

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,52 @@
 import React from 'react'
 import "./navbar.scss"
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import LanguageOutlinedIcon from '@mui/icons-material/LanguageOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import FullscreenExitOutlinedIcon from '@mui/icons-material/FullscreenExitOutlined';
+import ChatBubbleOutlineOutlinedIcon from '@mui/icons-material/ChatBubbleOutlineOutlined';
+import ListOutlinedIcon from '@mui/icons-material/ListOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      
+      <div className="wrapper">
+        <div className="search">
+          <input type="text" placeholder='Search...'/>
+          <SearchOutlinedIcon />
+        </div>
+        <div className="items">
+          <div className="item">
+            <LanguageOutlinedIcon className='icon'/>
+            English
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon className='icon'/>
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon className='icon'/>
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className='icon'/>
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className='icon'/>
+            <div className="counter">1</div>
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className='icon'/>
+            <div className="counter">2</div>
+          </div>
+          <div className="item">
+          <img src="https://img.freepik.com/free-vector/businessman-character-avatar-isolated_24877-60111.jpg?w=2000" 
+          alt="avatar" 
+          className='avatar'
+          />
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
