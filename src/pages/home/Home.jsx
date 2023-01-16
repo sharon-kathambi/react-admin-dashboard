f82d39f4cda64d34c4d91dfa72d9a37140dc5248
Sharon Mwenda <smkathambi@gmail.com>
2023-01-16 13:56:54 +0300

    building sidebar component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Sidebar from '../../components/sidebar/Sidebar'
 import './home.scss'
 
 const Home = () => {
   return (
     <div className='home'>
-      Home
+      <Sidebar />
+      <div className="homeContainer">container</div>  
     </div>
   )
 }
