e7b8041293a0440f3c4837ced22b0c360644a3ee
Sharon Mwenda <smkathambi@gmail.com>
2023-01-25 17:27:42 +0300

    add form input to new page component

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import './new.scss'
 
 const New = () => {
   return (
-    <div>
-      
+    <div className='new'>
+      <Sidebar />
+      <div className="newContainer">
+        <Navbar />
+        <div className="top">
+          <h1>Add new user</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img src='https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'/>
+          </div>
+          <div className="right">
+            <form>
+              <div className="formInput">
+                <label htmlFor='file'>
+                  Image: <DriveFolderUploadOutlinedIcon className="icon" />
+                  </label>
+                <input type='file' id='file'/>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
