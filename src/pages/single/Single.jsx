6bb2a884af24aef0fe1d0503090bcd23320d380f
Sharon Mwenda <smkathambi@gmail.com>
2023-01-25 16:34:46 +0300

    adding a single page component

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Sidebar from '../../components/sidebar/Sidebar'
 import './single.scss'
 
 const Single = () => {
   return (
-    <div>
+    <div className='single'>
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+      </div>
       
     </div>
   )
